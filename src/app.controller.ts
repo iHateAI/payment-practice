57a624db9a62b0f13a70285f71d9e36f8e1e49d1
HyunSeong <bolton7425@gmail.com>
2023-05-17 03:41:49 +0900

    FEAT: 결제사 승인 확인 API, 결제사 승인 실패 API 생성

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Render, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
 
-@Controller()
+@Controller('payment')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('index')
+  getHello(): any {
+    return { message: this.appService.getHello() };
+  }
+
+  @Post('auth')
+  paymentAuth(@Body() body: any, @Res() res: Response): any {
+    const { authResultCode, tid } = body;
+    // 결제사 인증 실패할 경우 예외 처리
+    if (authResultCode !== '0000') res.redirect(301, '/payment/fail');
+
+  }
+
+  @Get('fail')
+  @Render('fail')
+  failAuth(): any {
+    return {
+      errorMessage: '결제사 인증에 실패하였습니다.',
+    };
   }
 }
