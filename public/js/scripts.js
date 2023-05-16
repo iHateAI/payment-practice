774636a1c822aac69cecc0dfe078b9bc812070b0
HyunSeong <bolton7425@gmail.com>
2023-05-17 02:40:47 +0900

    FEAT: 결제창 호출 이벤트 생성

---
+++ public/js/scripts.js
@@ -0,0 +1,19 @@
+const paymentButton = document.querySelector('.payment-btn');
+
+const payment = (event) => {
+  const payload = {
+    clientId: 'S2_f9ee3fac82a345f6aa6db100e2dfedca',
+    method: 'card',
+    orderId: '306609f4-3d99-4b6f-9d61-0a429f01c66a',
+    amount: 100,
+    goodsName: '테스트 상품',
+    returnUrl: 'http://localhost:3000/payment/auth', //API를 호출할 Endpoint 입력
+    fnError: function (result) {
+      alert('개발자확인용 : ' + result.errorMsg + '');
+    },
+  };
+
+  AUTHNICE.requestPay(payload);
+};
+
+paymentButton.onclick = payment;
